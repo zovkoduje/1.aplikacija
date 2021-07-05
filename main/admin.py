87bc36daa16d55b283536cd9c82f184ffa5a0b31
Luka Cindric <lcindric9@gmail.com>
2021-07-05 18:17:08 +0200

    modeli

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from main.models import *
 
 # Register your models here.
+
+models = [Student , Predmet , Profesor]
+
+admin.site.register(models)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.db.models.fields import BooleanField
 
 # Create your models here.
+
+class Profesor(models.Model):
+    ime = models.CharField(max_length=20)
+    prezime = models.CharField(max_length=30)
+    email = models.EmailField()
+
+class Predmet(models.Model):
+    naziv_predmeta = models.CharField(max_length=30)
+    broj_ectsa = models.IntegerField()
+    semestar_predmeta = models.IntegerField()
+    obavezan_predmet = models.BooleanField()
+
+    nositelj = models.OneToOneField(Profesor , on_delete=models.CASCADE)
+
+class Student(models.Model):
+    ime = models.CharField(max_length=20)
+    prezime = models.CharField(max_length=30)
+    email = models.EmailField()
+    redovan_student = models.BooleanField()
+    broj_ostvarenih_ectsa = models.IntegerField()
+    semestar_studija = models.IntegerField()
+
+    predmet = models.ForeignKey(Predmet, on_delete=models.CASCADE)
+
+
+    
