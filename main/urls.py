477ec6d837744312fd0c0d0e507d2a6f4816a917
Luka Cindric <lcindric9@gmail.com>
2021-07-05 19:00:59 +0200

    templates

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from main.views import *
 
 urlpatterns = [
-    
+    path("" , homepage, name = "homepage"),
+    path("students" , StudentList.as_view())
 ]
--- main/views.py
+++ main/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.http.response import HttpResponse
+from django.shortcuts import redirect, render
+from main.models import *
+from django.views.generic import ListView
 
 # Create your views here.
+
+def homepage(request):
+    return HttpResponse("<h1> Hoempage </h1>")
+
+class StudentList(ListView):
+    model = Student
